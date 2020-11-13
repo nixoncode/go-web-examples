0f658a30f0a514934e7fdf30c6d29a6e927ebbf8
Nixon Kosgei <nkosgey6@gmail.com>
2020-11-13 12:50:49 +0300

    finished a simple http server with static files server

--- http-server/main.go
+++ http-server/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	
+
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":5050", nil)
 }
