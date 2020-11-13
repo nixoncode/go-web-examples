65927cb846f0c783896ccd25ae9bfb6102682802
Nixon Kosgei <nkosgey6@gmail.com>
2020-11-13 14:06:01 +0300

    numbered the folders in order

--- 3-routing-mux/main.go
+++ 3-routing-mux/main.go
@@ -13,8 +13,8 @@ func main() {
 		fmt.Fprint(w, "Welcome to site. Powered by gorilla mux route handler")
 	})
 
-	r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w,"Hello %s", mux.Vars(r)["name"])
+	r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %s", mux.Vars(r)["name"])
 	})
 
 	http.ListenAndServe(":5050", r)
